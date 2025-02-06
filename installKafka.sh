837351399f1c8bb381a87fc30eb0cbed78fe692d
vinaykumargudi <108074910+vinaykumargudi@users.noreply.github.com>
2025-02-06 22:58:30 +0530

    Update installKafka.sh

--- installKafka.sh
+++ installKafka.sh
@@ -19,6 +19,48 @@ fi
 
 # Install Kafka using Helm
 echo "Installing Kafka using Helm..."
+helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack
+
 helm install my-release oci://registry-1.docker.io/bitnamicharts/kafka -f https://raw.githubusercontent.com/vinaykumargudi/test/refs/heads/main/values.yaml
 
 echo "Kafka installation completed successfully!"
+echo "prometheus  installation started!"
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+helm install prometheus prometheus-community/prometheus
+
+
+kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-ext
+
+
+helm repo add grafana https://grafana.github.io/helm-charts 
+helm repo update
+
+helm install grafana grafana/grafana
+
+kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-ext
+
+
+kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | ForEach-Object { [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_)) }
+
+
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
+
+
+ ngrok config add-authtoken 2sfshrEkS9MtvHbAWkFLhmv6MzQ_6RrdL9q6xNbvmffPNsLS3
+
+
+kubectl port-forward svc/grafana 3000:80 &
+
+ngrok http http://localhost:3000 
+
+
+
+
+
